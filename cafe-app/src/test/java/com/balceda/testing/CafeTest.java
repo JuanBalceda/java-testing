cc5a87e095c38bd31ccd27181684ac91e525e836
Juan Balceda <juanbalceda@gmail.com>
2018-12-16 17:01:42 +0100

    Module III - Writing good tests: Working with Hamcrest

--- cafe-app/src/test/java/com/balceda/testing/CafeTest.java
+++ cafe-app/src/test/java/com/balceda/testing/CafeTest.java
@@ -1,18 +1,19 @@
 package com.balceda.testing;
 
-import junit.framework.TestCase;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 
 /**
  * Unit test for simple Cafe.
  */
 public class CafeTest {
 
-    private static final int REQUIRED_BEANS = CoffeeType.Espresso.getRequiredBeans();
+    private static final int ESPRESSO_BEANS = CoffeeType.Espresso.getRequiredBeans();
     private static final int NO_BEANS = 0;
     private static final int NO_MILK = 0;
 
@@ -34,9 +35,10 @@ public class CafeTest {
         // It is an espresso
         // No milk
         // We've got enough coffee
+        assertThat(coffee, hasProperty("beans",equalTo(ESPRESSO_BEANS)));
         Assert.assertEquals("Wrong coffee type", CoffeeType.Espresso, coffee.getType());
         Assert.assertEquals("Wrong amount of milk", NO_MILK, coffee.getMilk());
-        Assert.assertEquals("Wrong number of beans", REQUIRED_BEANS, coffee.getBeans());
+        Assert.assertEquals("Wrong number of beans", ESPRESSO_BEANS, coffee.getBeans());
     }
 
     @Test
@@ -68,6 +70,6 @@ public class CafeTest {
     }
 
     private void withBeans() {
-        cafe.restockBeans(REQUIRED_BEANS);
+        cafe.restockBeans(ESPRESSO_BEANS);
     }
 }
